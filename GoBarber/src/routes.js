35cf51ff4046620a3eeea1b820f636bf44279691
Bzll <cirocbizelli@gmail.com>
2019-10-16 14:34:29 -0400

    gostack

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -2,8 +2,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Those routes below will be validated by the middleware auth.js
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
