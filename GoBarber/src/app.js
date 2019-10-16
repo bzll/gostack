c7d28da6916c3d06bb679d85398afdd3743655b6
Bzll <cirocbizelli@gmail.com>
2019-10-15 23:31:16 -0400

    gostack

--- GoBarber/src/app.js
+++ GoBarber/src/app.js
@@ -1,10 +1,14 @@
+/* eslint-disable linebreak-style */
 /*
 const express = require('express');
 const routes = require('./routes');
 */
+// eslint-disable-next-line linebreak-style
 // If you install the dependence sucrase and nodemon, you can change the syntax of module importantion
 import express from 'express';
-import routes  from './routes';
+import routes from './routes';
+
+import './database';
 
 class App {
 	constructor() {
