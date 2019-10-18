9870c22d656d25a3ea88d966f79d24549e93b906
Bzll <cirocbizelli@gmail.com>
2019-10-17 20:16:38 -0400

    gostack

---
+++ GoBarber/src/config/multer.js
@@ -0,0 +1,17 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+	storage: multer.diskStorage({
+		destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+		filename: (req, file, cb) => {
+			crypto.randomBytes(16, (err, res) => {
+				if (err) return cb(err);
+
+				// example: u11u3g7138xeae.png
+				return cb(null, res.toString('hex') + extname(file.originalname));
+			});
+		},
+	}),
+};
