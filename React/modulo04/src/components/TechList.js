ee259b99cbd2dc785512ffc1ba16300858fbe310
Bzll <bzll926@gmail.com>
2019-10-29 22:45:28 -0400

    gostack

--- React/modulo04/src/components/TechList.js
+++ React/modulo04/src/components/TechList.js
@@ -1,77 +1,77 @@
 import React, { Component } from 'react';
 import TechItem from './TechItem'
 class TechList extends Component {
-	state = {
-		newTech: '',
-		techs: [
-		]
-	};
+    state = {
+        newTech: '',
+        techs: []
+    };
 
-	// Executado assim que o componente aparece em tela
-	componentDidMount(){
-		const techs = localStorage.getItem('techs')
-		if (techs) {
-			this.setState({
-				techs: JSON.parse(techs)
-			})
-		}
-	}
+    // Executado assim que o componente aparece em tela
+    componentDidMount() {
+        const techs = localStorage.getItem('techs')
+        if (techs) {
+            this.setState({
+                techs: JSON.parse(techs)
+            })
+        }
+    }
 
-	// Executado sempre que houver alteracoes nas props ou estado
-	componentDidUpdate(prevProps, prevState){
-		// this.props, this.state
-		if (prevState.techs !== this.state.techs) {
-			localStorage.setItem('techs', JSON.stringify(this.state.techs))
-		}
-	}
+    // Executado sempre que houver alteracoes nas props ou estado
+    componentDidUpdate(prevProps, prevState) {
+        // this.props, this.state
+        if (prevState.techs !== this.state.techs) {
+            localStorage.setItem('techs', JSON.stringify(this.state.techs))
+        }
+    }
 
-	// Executado quando o componente deixa de existir
-	componentWillUnmount(){
+    // Executado quando o componente deixa de existir
+    componentWillUnmount() {
 
-	}
+    }
 
-	// Uma funcao que precisa ter acesso ao elemento this, precisa ser uma arrow
-	handleInputChhange = e => {
-		this.setState({
-			newTech: e.target.value
-		})
-	}
+    // Uma funcao que precisa ter acesso ao elemento this, precisa ser uma arrow
+    handleInputChhange = e => {
+        this.setState({
+            newTech: e.target.value
+        })
+    }
 
-	handleSubmit = e => {
-		e.preventDefault();
+    handleSubmit = e => {
+        e.preventDefault();
 
-		this.setState({
-			techs: [... this.state.techs, this.state.newTech],
-			newTech: ''
-		})
-	}
+        this.setState({
+            techs: [...this.state.techs, this.state.newTech],
+            newTech: ''
+        })
+    }
 
-	handleDelete = (tech) => {
-		this.setState({ techs: this.state.techs.filter(t => t !== tech)})
+    handleDelete = (tech) => {
+        this.setState({ techs: this.state.techs.filter(t => t !== tech) })
 
-	}
+    }
 
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<ul>
-					{ this.state.techs.map(tech => (
-						<TechItem
-							key={tech}
-							tech={tech}
-							onDelete={() => this.handleDelete(tech)}
-						/>
-					))}
-				</ul>
-				<input
-					type="text"
-					onChange={this.handleInputChhange}
-					value={this.state.newTech}
-				/>
-				<button type="submit">Enviar</button>
-			</ form>
-		);
-	}
+    render() {
+        return ( <
+            form onSubmit = { this.handleSubmit } >
+            <
+            ul > {
+                this.state.techs.map(tech => ( <
+                    TechItem key = { tech }
+                    tech = { tech }
+                    onDelete = {
+                        () => this.handleDelete(tech) }
+                    />
+                ))
+            } <
+            /ul> <
+            input type = "text"
+            onChange = { this.handleInputChhange }
+            value = { this.state.newTech }
+            /> <
+            button type = "submit" > Enviar < /button> <
+            / form>
+        );
+    }
 }
 
 export default TechList;
